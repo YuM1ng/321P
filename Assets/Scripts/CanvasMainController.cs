84941a444daa6e44bd82c28b2476fac91b27246e
YuM1ng <kwongyuming17@gmail.com>
2022-12-06 22:03:43 +0800

    -Started new UIs in MainMenu scene
    -Added images for buttons
    
    -Each UI is made into prefab for loading through script in future

--- Assets/Scripts/CanvasMainController.cs
+++ Assets/Scripts/CanvasMainController.cs
@@ -12,6 +12,21 @@ public class CanvasMainController : MonoBehaviour
     private Animator m_storeSortAnimCont;
     private bool m_sortIsOpen = false;
 
+    [SerializeField]
+    private GameObject m_goProfile;
+    [SerializeField]
+    private GameObject m_goFAQ;
+    [SerializeField]
+    private GameObject m_goAbout;
+    [SerializeField]
+    private GameObject m_goCreate;
+    [SerializeField]
+    private GameObject m_goCreateAR;
+    [SerializeField]
+    private GameObject m_goStore;
+    [SerializeField]
+    private GameObject m_goMainUI;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -42,5 +57,40 @@ public class CanvasMainController : MonoBehaviour
             m_storeSortAnimCont.SetTrigger("SortClose");
             m_sortIsOpen = false;
         }
+    }
+    public void ProfileToFAQ()
+    {
+        m_goProfile.SetActive(false);
+        m_goFAQ.SetActive(true);
+    }
+    public void FAQToAbout()
+    {
+        m_goFAQ.SetActive(false);
+        m_goAbout.SetActive(true);
+    }
+    public void AboutToCreate()
+    {
+        m_goAbout.SetActive(false);
+        m_goCreate.SetActive(true);
+    }
+    public void CToCAR()
+    {
+        m_goCreate.SetActive(false);
+        m_goCreateAR.SetActive(true);
+    }
+    public void CreateARToStore()
+    {
+        m_goCreateAR.SetActive(false);
+        m_goStore.SetActive(true);
+    }
+    public void StoreToMAinUI()
+    {
+        m_goStore.SetActive(false);
+        m_goMainUI.SetActive(true);
+    }
+    public void MainUIToProfile()
+    {
+        m_goMainUI.SetActive(false);
+        m_goProfile.SetActive(true);
     }
 }
