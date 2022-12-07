c106887b56674dd3a287b9db42c76e87d2492e6d
YuM1ng <kwongyuming17@gmail.com>
2022-12-07 22:29:06 +0800

    -Some prefab changes and script changes

---
+++ Assets/Scripts/MainMenuLogic.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Prefab of the Start screen")]
+    private GameObject m_prefabStartScreen;
+    
+    [SerializeField]
+    [Tooltip("Prefab of the Intro screen")]
+    private GameObject m_prefabIntroScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
