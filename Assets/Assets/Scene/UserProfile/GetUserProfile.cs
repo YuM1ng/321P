eba52f3b5cf571a94da498c42e54f310e04b3e08
Syncrownite <104687003+Syncrownite@users.noreply.github.com>
2023-01-25 01:42:32 +0800

    Update #3

--- Assets/Assets/Scene/UserProfile/GetUserProfile.cs
+++ Assets/Assets/Scene/UserProfile/GetUserProfile.cs
@@ -11,7 +11,8 @@ using UnityEngine.SceneManagement;
 
 public class GetUserProfile : MonoBehaviour
 {
-    
+    [SerializeField] 
+        private TextMeshProUGUI gender;
     
     // Start is called before the first frame update
     void Start()
@@ -23,39 +24,16 @@ public class GetUserProfile : MonoBehaviour
         // string serializedSessionId = JsonConvert.SerializeObject(Obj);
         Debug.Log(session_id);
         StartCoroutine(RetrieveUserProfile(session_id));
+        
+        
        
         
     }
+    
 
      private IEnumerator RetrieveUserProfile(string session_id)
     {
-        
-    //     string json = JsonConvert.SerializeObject(session_id);
-    //     UnityWebRequest request = new UnityWebRequest("https://lunar-byte-371808.et.r.appspot.com/api/fetchuserprofilesid", "POST");
-    //     // UnityWebRequest request = UnityWebRequest.Post("https://lunar-byte-371808.et.r.appspot.com/api/fetchuserprofilesid", "POST");
-    //     // DownloadHandler objects are helper objects. 
-    //     // When attached to a UnityWebRequest, they define how to handle HTTP response body data received from a remote server. 
-    //     // Generally, they are used to buffer, stream and/or process response bodies
-    //     byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
-	// 	request.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
-	// 	request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-    //     request.SetRequestHeader("Content-Type", "application/json");
-    //     request.method = UnityWebRequest.kHttpVerbPOST;
-    //     Debug.Log("this the json : " + json);
-    //     yield return request.SendWebRequest();
-        
-    //     if (request.isNetworkError || request.isHttpError)
-    //     {
-    //         Debug.Log(request.downloadHandler.text);
-    //         Debug.LogError("Error retrieving user profile: " + request.error);
-    //     }
-    //     else
-    //     {
-    //         var userProfile = JsonUtility.FromJson<UserProfile>(request.downloadHandler.text);
-    //         Debug.Log("User Profile :" + userProfile);
-    
-    // }
-            WWWForm form = new WWWForm();
+        WWWForm form = new WWWForm();
         form.AddField("session_id", session_id);
 
         UnityWebRequest www = UnityWebRequest.Post("https://lunar-byte-371808.et.r.appspot.com/api/fetchuserprofilesid", form);
@@ -68,6 +46,7 @@ public class GetUserProfile : MonoBehaviour
         else
         {
             Debug.Log(www.downloadHandler.text);
+            gender.text = www.downloadHandler.text;
              
 
         }
