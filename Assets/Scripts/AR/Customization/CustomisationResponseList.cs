99a64c99bf02ea51fb0a919af4c8111e372d6121
songx2 <35437603+ckaisong@users.noreply.github.com>
2023-02-17 14:37:00 +0800

    push
    ..

--- Assets/Scripts/AR/Customization/CustomisationResponseList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class CustomisationResponseList
-{
-   public List<CustomisationResponse> custList;
-}
